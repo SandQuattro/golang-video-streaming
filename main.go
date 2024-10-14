eeec9aa6590afa90b3ea3892786830439fc82705
Igor <SandQuattro@gmail.com>
2024-10-14 10:45:50 +0300

    chore: fix

--- main.go
+++ main.go
@@ -15,10 +15,14 @@ const sourceFile = "raceboat.mp4"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Welcome to our video streaming platform!")
+		_, err := fmt.Fprintln(w, "Welcome to our video streaming platform!")
+		if err != nil {
+			log.Fatal(err)
+		}
 	})
+
 	http.HandleFunc("/stream", handleStream)
-	fmt.Println("Starting server on :8080")
+	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
